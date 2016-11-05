f7fc9eaa7c06107ba961f4f40ab0a54420565b90
Shirshendu Mukherjee <m-shirshendu@users.noreply.github.com>
2016-11-05 18:38:36 +0530

    Update output page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
 (function() {
+  var spv;
 
+  spv = function() {
+    var w;
+    w = $('.swiper-container').width();
+    if (w <= 425) {
+      return 1;
+    }
+    if (w <= 1024) {
+      return 2;
+    }
+    return 3;
+  };
+
+  $(function() {
+    var mySwiper;
+    return mySwiper = new Swiper('.swiper-container', {
+      centerdSlides: true,
+      a11y: true,
+      speed: 600,
+      effect: 'coverflow',
+      slidesPerView: spv(),
+      spaceBetween: 0,
+      autoHeight: true,
+      paginationClickable: true,
+      autoplay: 4000,
+      keyboardControl: true,
+      coverflow: {
+        slideShadows: false
+      },
+      loop: true,
+      pagination: '.swiper-pagination',
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev'
+    });
+  });
 
 }).call(this);
